bf38658045d1c69bb0770bd8b8e1efe58205cea1
Devanm15 <devan.mckay@gmail.com>
2019-08-29 21:18:10 +0000

    created cultures db table

---
+++ db/migrate/20190829210709_create_cultures.rb
@@ -0,0 +1,10 @@
+class CreateCultures < ActiveRecord::Migration[5.2]
+  def change
+    create_table :cultures do |t|
+      t.string :name
+      t.text :description
+
+      t.timestamps
+    end
+  end
+end
