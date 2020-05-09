97bbe42a06bb53ab449a993ef646dd5ef44757e2
Devanm15 <devan.mckay@gmail.com>
2020-05-09 01:20:41 +0000

    Create User Login form

--- client/src/Components/NavBar.js
+++ client/src/Components/NavBar.js
@@ -1,23 +1,39 @@
 import React, { useEffect, useState, Component } from "react";
 import { Redirect } from "react-router-dom";
 import "antd/dist/antd.css";
-import { Menu, Row, Button } from "antd";
-import axios from "axios";
+import { Menu, Row, Modal } from "antd";
+import User from "./User.js";
 
 function NavBar(props) {
+  const [show, setModal] = useState(false);
+
+  function showModal() {
+    setModal(true);
+  }
+  function handleCancel() {
+    setModal(false);
+  }
+  function handleOk() {
+    setModal(false);
+  }
   return (
     <div className="Menu">
       <Row>
         <h1>Earth Medicine App</h1>
-        <Menu
-          // onClick={this.handleClick}
-          // selectedKeys={[this.state.current]}
-          mode="horizontal"
-        >
-          <Menu.Item key="login">Login | Register</Menu.Item>
-          {/* <Menu.Item key="app" disabled></Menu.Item> */}
+        <Menu mode="horizontal">
+          <Menu.Item key="login" onClick={showModal}>
+            Login | Register
+          </Menu.Item>
         </Menu>
       </Row>
+      <Modal
+        visible={show}
+        onCancel={handleCancel}
+        onOk={handleOk}
+        destroyOnClose={true}
+      >
+        {<User />}
+      </Modal>
     </div>
   );
 }
---
+++ client/src/Components/User.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Form, Input, Button, Checkbox } from "antd";
+
+function User(props) {
+  return (
+    <Form name="basic" initialValues={{ remember: false }}>
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: "Please input your username!" }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: "Please input your password!" }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item name="remember" valuePropName="checked">
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+    </Form>
+  );
+}
+
+export default User;
