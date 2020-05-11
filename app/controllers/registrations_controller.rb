b7310d9a9a041e13bb1fccd9155455f02b7bfe19
Devanm15 <devan.mckay@gmail.com>
2020-05-11 01:20:08 +0000

    create registration controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,16 @@
+Class RegistrationsController < ApplicationController
+    def create
+        user = User.create!(
+            email: params['user']['email'],
+            password: params['user']['password'],
+            password_confirmation: params['user']['password_confirmation'],
+        )
+        if user
+            session[:user_id] = user.user_id
+            render json: {
+                status: :created,
+                user: user
+            }
+            renderjson: {status: 500}
+    end 
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :sessions, only: [:create]
+  resources :registrations, only: [:create]
+  delete :logout, to: "sessions#logout"
+  get :logout_in, to "sessions#logged_in"
   namespace :api, defaults: { format: 'json' } do # /api/data
     get '/index', to: 'cultures#index'
     root 'cultures#index'
     resources :cultures, :locations, only: [:index, :show]
   end
   
-  # namespace :api, defaults: { format: 'json' } do # /api/data
-  #   get '/index/locations', to: 'locations#index'
-  #   root 'locations#index'
-  #   resources :locations, only: [:index, :show]
-  # end
-
   # get '*path', to: "static_pages#fallback_index_html", constraints: ->(request) do
   #   !request.xhr? && request.format.html?
   # end
