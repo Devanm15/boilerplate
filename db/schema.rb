fae91ec506428d5a841cd067d2fb7ac3f1cff27c
Devanm15 <devan.mckay@gmail.com>
2020-05-11 00:41:05 +0000

    User database, route, model  created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_03_235333) do
+ActiveRecord::Schema.define(version: 2020_05_11_000108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,6 +32,13 @@ ActiveRecord::Schema.define(version: 2019_09_03_235333) do
   create_table "locations", force: :cascade do |t|
     t.float "latitude"
     t.float "longitude"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
