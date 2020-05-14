5bc99878a8cd798ab616b0b9510c74a65e958af4
Devanm15 <devan.mckay@gmail.com>
2020-05-14 04:21:19 +0000

    registration form created

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ ruby '2.6.2'
 
 gem 'react-rails'
 
+gem 'awesome_print'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.3', '>= 5.2.2.1'
--- client/src/Components/User.js
+++ client/src/Components/User.js
@@ -1,10 +1,14 @@
-import React, { useState, useRef, createRef } from "react";
-import { Form, Input, Button, Checkbox } from "antd";
+import React, { useState, useRef } from "react";
+import { Button, Checkbox, Form, Input } from "antd";
+import { useForm } from "react-hook-form";
+import axios from "axios";
 
 function User(props) {
   const [isHidden, setIsHidden] = useState(true);
   const emailValue = useRef();
   const passwordValue = useRef();
+  const { register, handleSubmit, errors, getValues } = useForm();
+
   function ToggleLogin() {
     setIsHidden(true);
   }
@@ -13,10 +17,37 @@ function User(props) {
     setIsHidden(false);
   }
 
-  function handleLoginInput(e) {
+  const onSubmit = data => {
+    axios
+      .post(
+        "http://localhost:3000/registrations",
+        {
+          user: {
+            email: data.email,
+            password: data.password,
+            password_confirmation: data.password_confirmation
+          }
+        },
+        { withCredentials: true }
+      )
+      .then(response => {
+        console.log("registration res", response);
+      })
+      .catch(error => {
+        console.log("reg error", error);
+      });
+    event.preventDefault();
+  };
+
+  function handleLoginInput() {
     emailValue.current.focus();
     passwordValue.current.focus();
   }
+
+  function handleChange(event) {
+    console.log(event.target);
+  }
+
   return (
     <div className="user-credentials">
       <div className="Login-Register-Buttons">
@@ -77,67 +108,77 @@ function User(props) {
           </Form>
         </div>
       )}
+
       {!isHidden && (
         <div className="Register">
-          <Form name="Register" initialValues={{ remember: false }}>
-            <Form.Item
-              label="Username"
-              name="username"
-              rules={[
-                {
+          <form onSubmit={handleSubmit(onSubmit)} className="Register-form">
+            <div className="Reg-Email">
+              <label>Email: </label>
+              <input
+                name="email"
+                type="email"
+                ref={register({ required: true })}
+                onChange={handleChange}
+              />
+              <div className="error-message">
+                {errors.email && "Email Required"}
+              </div>
+            </div>
+            <div className="reg-password">
+              <label>Password: </label>
+              <input
+                name="password"
+                type="password"
+                ref={register({ required: true, minLength: 8 })}
+                onChange={handleChange}
+              />
+              <div className="error-message">
+                {" "}
+                {errors.password && errors.password.type === "required" && (
+                  <p>Your input is required</p>
+                )}
+                {errors.password && errors.password.type === "minLength" && (
+                  <p>Your password needs to be a minimum of 8 characters</p>
+                )}
+              </div>
+            </div>
+            <div className="reg-password-confirmation">
+              <label>Password Confirmation: </label>
+              <input
+                name="passwordConfirmation"
+                type="password"
+                ref={register({
                   required: true,
-                  message: "Please enter a username!"
-                },
-                {
-                  min: 5,
-                  max: 20,
-                  message: "Your password must be between 5-20 characters long"
-                }
-              ]}
-            >
-              <Input />
-            </Form.Item>
-            <Form.Item
-              label="Email"
-              name="email"
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your Email!"
-                },
-                {
-                  type: "email",
-                  message: "Oops! that is not a valid email"
-                }
-              ]}
-            >
-              <Input />
-            </Form.Item>
+                  validate: {
+                    matchesPreviousPassword: value => {
+                      const { password } = getValues();
+                      return (
+                        password === value || <p>Passwords Should Match</p>
+                      );
+                    }
+                  }
+                })}
+                onChange={handleChange}
+              />
+              <div className="error-message">
+                {errors.passwordConfirmation && (
+                  <p>{errors.passwordConfirmation.message}</p>
+                )}
+              </div>
+            </div>
 
-            <Form.Item
-              label="Password"
-              name="password"
-              rules={[
-                { required: true, message: "Please input your password!" },
-                {
-                  min: 8,
-                  message: "Your password must be atleast 8 characters long"
-                }
-              ]}
+            <Checkbox>Remember me</Checkbox>
+
+            <Button
+              type="primary"
+              htmlType="submit"
+              onClick={() => {
+                onSubmit;
+              }}
             >
-              <Input.Password />
-            </Form.Item>
-
-            <Form.Item name="remember" valuePropName="checked">
-              <Checkbox>Remember me</Checkbox>
-            </Form.Item>
-
-            <Form.Item>
-              <Button type="primary" htmlType="submit">
-                Submit
-              </Button>
-            </Form.Item>
-          </Form>
+              Submit
+            </Button>
+          </form>
         </div>
       )}
     </div>
