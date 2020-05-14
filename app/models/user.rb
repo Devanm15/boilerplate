5d776e8a84863b535395420192d2187d483c7a4e
Devanm15 <devan.mckay@gmail.com>
2020-05-14 20:08:58 +0000

    added unique validation to email in model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
+    validates :email, presence: true, uniqueness: { message: "This email has already been used to create an account"}
     has_secure_password
 
-    validates_presence_of :email 
+   
 end
