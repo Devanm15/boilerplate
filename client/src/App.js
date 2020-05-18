c845d24649872d21758dda769bacc5bcd782a55b
Devanm15 <devan.mckay@gmail.com>
2020-05-18 01:16:00 +0000

    added cookies to user login

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import Cookies from "js-cookie";
 import "./App.css";
 import "antd/dist/antd.css";
 import MapContainer from "./Components/MapContainer.js";
@@ -10,12 +11,12 @@ import { Button } from "antd";
 function App(props) {
   const [cultures, setCultures] = useState([]);
   const [showCultureMarkers, setShowCultureMarkers] = useState(false);
-  const [culturesButtonClick, setCulturesButtonClick] = useState(false);
-  const [formButtonClick, setFormButtonClick] = useState(false);
-  const [clickCount, setClickCount] = useState(0);
+  const [showCultureComponent, setShowCultureComponent] = useState(false);
+  const [showFormComponent, setShowFormComponent] = useState(false);
   const [cultureId, setCultureId] = useState();
   const [loggedInStatus, setLoggedInStatus] = useState("Not Logged In");
   const [loggedInUser, setUser] = useState();
+  let userEmail = Cookies.get("email");
 
   useEffect(
     state => {
@@ -24,23 +25,36 @@ function App(props) {
           cultures: response.data
         });
       });
+      axios
+        .get("http://localhost:3000/api/logged_in", {
+          withCredentials: true
+        })
+        .then(response => {
+          if (response.data.user.email === userEmail) {
+            setLoggedInStatus("Logged In");
+            setUser(response.data.user.username);
+          } else {
+            setLoggedInStatus("Not Logged In");
+            setUser();
+          }
+        })
+        .catch(error => {
+          console.log("check login error", error);
+        });
     },
 
     [props]
   );
 
-  function checkLoginStatus() {
+  function handleLogin() {
     axios
       .get("http://localhost:3000/api/logged_in", {
         withCredentials: true
       })
       .then(response => {
-        if (response.data.logged_in && loggedInStatus === "Not Logged In") {
+        if (response.data.user.email === userEmail) {
           setLoggedInStatus("Logged In");
           setUser(response.data.user.username);
-        } else if (!response.data.logged_in && loggedInStatus === "Logged In") {
-          setLoggedInStatus("Not Logged In");
-          setUser();
         }
       })
       .catch(error => {
@@ -48,39 +62,34 @@ function App(props) {
       });
   }
 
-  function handleLogin() {
-    setLoggedInStatus("Logged In");
-  }
-
   function handleLogout() {
     setLoggedInStatus("Not Logged In");
     setUser();
+    Cookies.remove("email");
   }
 
   function handleCultureClick(e) {
-    if (clickCount == 0) {
-      setCulturesButtonClick(true);
+    if (showCultureComponent === false) {
+      setShowCultureComponent(true);
       setShowCultureMarkers(true);
-      setClickCount(1);
-    } else if (clickCount == 1) {
-      setCulturesButtonClick(false);
-      setClickCount(0);
+      setShowFormComponent(false);
+    } else {
+      setShowCultureComponent(false);
     }
   }
   function handleFormClick(e) {
-    if (clickCount == 0) {
-      setFormButtonClick(true);
-      setClickCount(1);
-    } else if (clickCount == 1) {
-      setFormButtonClick(false);
-      setClickCount(0);
+    setShowCultureComponent(false);
+    if (showFormComponent === false) {
+      setShowFormComponent(true);
+    } else {
+      setShowFormComponent(false);
     }
   }
 
   function handleMouseLeave(e) {
-    if (culturesButtonClick == true) {
+    if (showCultureComponent == true) {
     }
-    if (culturesButtonClick == false) {
+    if (showCultureComponent == false) {
       setShowCultureMarkers(false);
     }
   }
@@ -90,7 +99,6 @@ function App(props) {
 
   return (
     <div className="App">
-      {checkLoginStatus()}
       <Navbar
         loggedInStatus={loggedInStatus}
         handleLogin={handleLogin}
@@ -117,8 +125,8 @@ function App(props) {
       />
 
       <InfoContainer
-        showCultureComponent={culturesButtonClick}
-        showFormComponent={formButtonClick}
+        showCultureComponent={showCultureComponent}
+        showFormComponent={showFormComponent}
         cultureClickHandler={onCultureClick}
         loggedInStatus={loggedInStatus}
       />
