2400980dbb97105b6cc132c3e54e772d0c4254c1
Devanm15 <devan.mckay@gmail.com>
2020-08-31 15:44:59 +0000

    testing password presence and length

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
     validates :email, presence: true, uniqueness: { message: "This email has already been used to create an account"}
+    validates :password, presence: true, length: {minimum: 8}
     has_secure_password
 
    
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,15 +2,19 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "is valid with valid attributes" do
-    user = User.new(username: "test", first_name: "test", last_name: "test", email: "test@me.ca", password_digest: "87654321")
+    user = User.new(username: "test", first_name: "test", last_name: "test", email: "test@me.ca", password: "87654321")
     expect(user).to be_valid 
   end
    it "is not valid without an email" do
-    user = User.new(username: "test", first_name: "test", last_name: "test", password_digest: "87654321")
+    user = User.new(username: "test", first_name: "test", last_name: "test", password: "87654321")
     expect(user).to be_invalid 
   end
   it "is not valid without a password" do
     user = User.new(username: "test", first_name: "test", last_name: "test", email:"test@me.ca")
+    expect(user).to be_invalid 
+  end
+  it "is not valid without a password of 8 characters" do
+    user = User.new(username: "test", first_name: "test", last_name: "test", email:"test@me.ca", password:"345")
     expect(user).to be_invalid 
   end
 end
