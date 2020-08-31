c72da60b00c1499554786010663d51d07170feef
Devanm15 <devan.mckay@gmail.com>
2020-08-31 14:57:35 +0000

    testing user email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,11 +6,11 @@ RSpec.describe User, type: :model do
     expect(user).to be_valid 
   end
    it "is not valid without an email" do
-    user = User.new(username: "test", first_name: "test", last_name: "test", email:"test@me.ca", password_digest: "87654321")
-    expect(user).to be_valid 
+    user = User.new(username: "test", first_name: "test", last_name: "test", password_digest: "87654321")
+    expect(user).to be_invalid 
   end
   it "is not valid without a password" do
-    user = User.new(username: "test", first_name: "test", last_name: "test", email:"test@me.ca", password_digest:"87654321")
-    expect(user).to be_valid 
+    user = User.new(username: "test", first_name: "test", last_name: "test", email:"test@me.ca")
+    expect(user).to be_invalid 
   end
 end
