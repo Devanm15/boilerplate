c54efc67ae1c3bfbc476ea0440f49ecc6287cc66
Devanm15 <devan.mckay@gmail.com>
2020-09-11 17:16:18 +0000

    validate uniqueness of email and username

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,6 +9,11 @@ RSpec.describe User, type: :model do
     user = User.new(username: "test", first_name: "test", last_name: "test", password: "87654321")
     expect(user).to be_invalid 
   end
+  
+  describe "is not valid without a unique email" do
+    it { should validate_uniqueness_of(:email).scoped_to(:username) } 
+  end
+
   it "is not valid without a password" do
     user = User.new(username: "test", first_name: "test", last_name: "test", email:"test@me.ca")
     expect(user).to be_invalid 
