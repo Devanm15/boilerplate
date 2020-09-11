26aa4f8e15511f516cf6fe018e00d38f57285384
Devanm15 <devan.mckay@gmail.com>
2020-09-11 17:28:48 +0000

    test culture model source must be present

--- spec/models/culture_spec.rb
+++ spec/models/culture_spec.rb
@@ -2,5 +2,15 @@ require 'rails_helper'
 
 RSpec.describe Culture, type: :model do
   it { should have_and_belong_to_many(:locations)} 
+
+  it "is not valid without a name" do
+    culture = Culture.new(name: "", source: "test")
+    expect(culture).to be_invalid 
+  end
+
+  it "is not valid without a source" do
+    culture = Culture.new(name: "test", source: "")
+    expect(culture).to be_invalid 
+  end
   
 end
