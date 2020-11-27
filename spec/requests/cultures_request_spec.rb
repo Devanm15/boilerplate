1c25a7dd668e8143997abe0274bfd51067e7a6e5
Devanm15 <devan.mckay@gmail.com>
2020-11-27 20:24:17 +0000

    testing cultures request

--- spec/requests/cultures_request_spec.rb
+++ spec/requests/cultures_request_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "Cultures", type: :request do
-
+    describe "GET index" do
+        it "assigns @cultures" do
+            culture = Culture.create
+            get /culture/index
+            expect(assigns(:cultures)).to eq([culture])
+        end
+    end
 end
